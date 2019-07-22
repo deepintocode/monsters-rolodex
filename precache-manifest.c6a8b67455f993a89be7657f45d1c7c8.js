98383182ffb595b5cd4b2b9f845e93f88c4fadfa
deepintocode <todorare@gmail.com>
2019-07-22 14:27:40 +0100

    Updates

---
+++ precache-manifest.c6a8b67455f993a89be7657f45d1c7c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625c2932308a393405033e56c1b77def",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1c938d5954018145a92f",
+    "url": "/monsters-rolodex/static/css/main.30477847.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1c938d5954018145a92f",
+    "url": "/monsters-rolodex/static/js/main.39c65c5a.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
